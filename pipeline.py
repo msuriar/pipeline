e20294d0a0407f4257bf1ff6f9f819c4d85441f2
Murali Suriar <code@suriar.com>
2014-05-04 16:03:11 +0100

    Create pipeline class.

--- pipeline.py
+++ pipeline.py
@@ -20,6 +20,36 @@ def pipelineprint(pipeline):
   totals = [ s.total for s in pipeline ]
   print ' ---> '.join([str(t) for t in totals])
 
+
+class Pipeline(object):
+  """Pipeline class."""
+  def __init__(self, length, minwork=1, maxwork=7):
+    self.length = length
+    self.minwork = minwork
+    self.maxwork = maxwork
+    self.stations = self.__buildpipeline()
+
+  def __buildpipeline(self):
+    """Construct pipeline."""
+    pipeline = [None]*self.length
+    pipeline[-1] = Station(sink=True)
+    for i in reversed(range(1, self.length - 1)):
+      pipeline[i] = Station(succ=pipeline[i+1])
+    pipeline[0] = Station(src=True, succ=pipeline[1])
+    return pipeline
+
+  def tick(self):
+    """Advance pipeline."""
+    for stations in self.stations:
+      stations.work()
+
+  def dump(self):
+    """Dump pretty printed pipeline."""
+
+    totals = [str(s.total) for s in self.stations]
+    totals[0] = ">>>"
+    print ' ---> '.join(totals)
+
 class Station(object):
   """Station is a class representing each workstation."""
   def __init__(self, src=False, sink=False, succ=None):
